e0494e6b4096cd4dafaa222d0ad0b4c492594357
Saumy Pandey <saumypandey745@gmail.com>
2026-08-21 17:22:34 +0530

    fix: update EmailJS Service ID to service_fg0mp0c and align template variables

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,6 +1,6 @@
 import emailjs from '@emailjs/browser';
 
-const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID || 'service_3990815';
+const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID || 'service_fg0mp0c';
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID || 'template_5gpxdjs';
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY || 'lyT5DJ-2Mnd8YzyZ_';
 
